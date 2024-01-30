620cb080e0be412d29366f206fe1ef5a9b1b1db7
xMAXVADERxx <42118429+xMAXVADERxx@users.noreply.github.com>
2024-01-30 23:09:36 +0000

    Added Core Framework
    Added Framework for all core API functions / bindings to the T212 API, attempting to match commands where possible, and using internal systems where needed. FUNCTIONS DO NOT WORK

--- api.py
+++ api.py
@@ -88,7 +88,7 @@ class Interface():
         return self.__EXCHANGES
     
     def refreshInstruments(self):
-        url = self.URL_Base + self.apiDict["INSTRUMENT"]
+        url = self.URL_Base + self.apiDict["INSTRUMENTS"]
 
         headers = {
             "Authorization" : self.__API_KEY
@@ -103,3 +103,108 @@ class Interface():
     
     def getInstruments(self):
         return self.__INSTRUMENTS
+
+    def refreshPies(self):
+        url = self.URL_Base + self.apiDict["PIES"]
+
+        headers = {
+            "Authorization" : self.__API_KEY
+        }
+
+        data = self.queueRequest(url, headers)
+
+        if data.status_code == 200:
+            self.__PIES = data.json()
+        
+        return data
+    
+    def getPies(self):
+        return self.__PIES
+
+    def getPieById(self, id=0):
+        url = self.URL_Base + self.apiDict["PIES"]
+        url += f'/{id}'
+
+        headers = {
+            "Authorization" : self.__API_KEY
+        }
+
+        data = self.queueRequest(url, headers)
+
+        if data.status_code == 200:
+            return data.json()
+        return
+
+    def createPie(self):
+        pass
+
+    def deletePie(self):
+        pass
+
+    def updatePie(self):
+        pass
+
+    def refreshEquity(self):
+        url = self.__URL_BASE + self.apiDict["EQUITY_ORDERS"]
+
+        headers = {
+            "Authorization" : self.__API_KEY
+        }
+
+        data = self.queueRequest(url, headers)
+
+        if data.status_code == 200:
+            self.__EQUITY_ORDERS = data.json()
+        
+        return data
+
+    def getEquityOrders(self):
+        return self.__EQUITY_ORDERS
+    
+    def placeLimitOrder(self):
+        pass
+
+    def placeMarketOrder(self):
+        pass
+
+    def placeStopOrder(self):
+        pass
+
+    def placeStopLimitOrder(self):
+        pass
+
+    def cancelByID(self):
+        pass
+
+    def fetchByID(self):
+        pass
+
+    def fetchAccountCash(self):
+        pass
+
+    def fetchAccountMetadata(self):
+        pass
+
+    def fetchAllOpenPositions(self):
+        pass
+
+    def fetchSpecificPosition(self):
+        pass
+
+    def refreshHistoricalOrderData(self):
+        pass
+
+    def getHistoricalOrderData(self):
+        pass
+
+    def getPaidOutDividends(self):
+        pass
+
+    def getExportsList(self):
+        pass
+
+    def getExportCSV(self):
+        pass
+
+    def getTransactionList(self):
+        pass
